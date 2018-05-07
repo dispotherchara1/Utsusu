f3cd4d7d661f558d3884fc70ee5122350c6c636d
Rikiya Yamamoto <nkc.yamamoto.rikiya@gmail.com>
2018-05-07 15:00:25 +0900

    タイマー()が出来上がりました

---
+++ Utsusu/Assets/YamamotoOnly/timer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+    public Text time;
+    bool timeover = false;
+    float timecounter=30;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time.text = timecounter.ToString("F0");
+        if (timecounter>=0) { Encounttimer(-Time.deltaTime); }
+
+        if (timeover == true) { Debug.Log("trueになっちゃったよ"); }
+	}
+
+    public void Encounttimer (float timer){
+
+        if (timecounter>=0)
+        {
+            timecounter+=timer;
+        }
+
+        else { timeover = true; }
+        }
+
+
+}
