16c28dcad6a64b831b12e3d6d7d3fe738322d73d
Eita Honjo <nkc.eita.honjo@gmail.com>
2018-05-07 14:58:01 +0900

    playercontrollerを追加

---
+++ Utsusu/Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 teleportation = new Vector2(50.0f, 0.0f);
+    // Use this for initialization
+    void Start ()
+    {
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Vector2 pos = transform.position;// 現在位置をposに代入
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            pos += teleportation * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            pos += teleportation * 2 * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            pos += teleportation * 3 * Time.deltaTime;
+        }
+        transform.position = pos ;//現在の位置にteleportationの移動距離を加算
+    }
+}
