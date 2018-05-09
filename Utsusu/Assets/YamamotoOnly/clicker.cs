36e1fa72445f1d7edfb60d97f78e03818c0b135d
Rikiya Yamamoto <nkc.yamamoto.rikiya@gmail.com>
2018-05-09 14:34:22 +0900

    クリッカーゲームの断片を作った

---
+++ Utsusu/Assets/YamamotoOnly/clicker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class clicker : MonoBehaviour {
+
+    bool click = true;
+    public Text print;
+    float page;
+	
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) { printer(+1);}
+        print.text =page.ToString("F0");
+	}
+
+    public void printer (float printed)
+    {
+        page += printed; 
+    }
+
+}
