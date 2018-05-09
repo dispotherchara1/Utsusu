6b65fb88dc4cecd4c94864943323884c967326fc
Rikiya Yamamoto <nkc.yamamoto.rikiya@gmail.com>
2018-05-09 17:00:47 +0900

    なぜかクリッカーゲームが大きく進んだ。２マス進む。

--- Utsusu/Assets/YamamotoOnly/clicker.cs
+++ Utsusu/Assets/YamamotoOnly/clicker.cs
@@ -1,27 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class clicker : MonoBehaviour {
 
-    bool click = true;
-    public Text print;
-    float page;
-	
-    // Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetMouseButtonDown(0)) { printer(+1);}
-        print.text =page.ToString("F0");
-	}
+    public static float page=0;
 
-    public void printer (float printed)
+    float famiport=1,famiport_yen=100,famicount=1;
+
+    private void Start()
+    {    }
+    private void Update()
     {
-        page += printed; 
+        page +=(famicount-1) *Time.deltaTime;
     }
 
+    private void Awake()
+    {
+        //イベント作成
+        EventTrigger.Entry pressDown = new EventTrigger.Entry();
+        pressDown.eventID = EventTriggerType.PointerDown;       //イベントのタイプ設定
+        pressDown.callback.AddListener((data) => { Page_masu(); });   //メソッド登録
+
+        //イベントトリガーにイベント追加
+        //GetComponent<EventTrigger>().triggers.Add(pressDown);
+    }
+    public void famiport_get()
+    {
+        if (page >= famiport_yen)
+        {
+            page -= famiport_yen;
+            famicount++;
+            famiport_yen = (famiport_yen * 1.1f);
+        }
+    }
+
+    public void Page_masu()
+    {
+            page += 1;        
+    }
 }
---
+++ Utsusu/Assets/YamamotoOnly/counter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class counter : MonoBehaviour {
+    
+    public Text printer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        printer.text = clicker.page.ToString("F0");
+    }
+}
