afb6880971c9018041cd8cbc48290541ed719c1d
Eita Honjo <nkc.eita.honjo@gmail.com>
2018-05-11 15:11:59 +0900

    今は使えないので次の時に修正します。

--- Utsusu/Assets/PlayerController.cs
+++ Utsusu/Assets/PlayerController.cs
@@ -1,49 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
-
 public class PlayerController : MonoBehaviour
 {
+    Gauge gauge;
     Vector2 teleportation = new Vector2(50.0f, 0.0f);
-    bool gauge = false;
-    float timeCount = 0.0f;
-    // Use this for initialization
-    void Start ()
-    {
-    }
+    float gettime = 0.0f;
 
     // Update is called once per frame
     void Update ()
     {
-        Vector2 pos = transform.position;// 現在位置をposに代入
+        Vector2 pos = transform.position;                            // 現在位置をposに代入
+        //if(gauge.GetTimeCount() != 0.0f)
+        {
+           // pos += teleportation * gauge.GetTimeCount(gettime) * Time.deltaTime;//移動計算式
+        }
+        transform.position = pos;                           //現在の位置にteleportationの移動距離を加算
 
-        if (Input.GetKey(KeyCode.B))//Bボタンを押している間ゲージがtrue(ゲージが動く)
-        {
-            gauge = true;
-        }
-        else
-        {
-            gauge = false;//押してないときfalse
-        }
-
-        if (gauge)
-        {
-            timeCount += 3 * Time.deltaTime;
-            if (timeCount >= 5.0f)//ゲージの最大値
-            {
-                timeCount = 0.0f;//ゼロに戻る
-            }
-            //Debug.Log("timeCont " + timeCount);
-        }
-        else      //Bボタンを離したときplayerが動く
-        {
-            pos += teleportation * timeCount * Time.deltaTime;//移動計算式
-            timeCount = 0.0f;                                 //falseに戻したときに初期化する
-        }
-        //Debug.Log("gauge " + gauge);
-        //Debug.Log("timeCont " + timeCount);
-
-        transform.position = pos;//現在の位置にteleportationの移動距離を加算
-        /*
+        /*ボタンで移動距離が違う
         Vector2 pos = transform.position;// 現在位置をposに代入
 
         if (Input.GetKeyDown(KeyCode.B))
