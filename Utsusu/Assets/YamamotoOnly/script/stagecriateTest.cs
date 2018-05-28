85d9f40a71e55dd58ec9454bc5f9fd8b0e7300cb
Rikiya Yamamoto <nkc.yamamoto.rikiya@gmail.com>
2018-05-28 14:49:33 +0900

    変更なし

--- Utsusu/Assets/YamamotoOnly/script/stagecriateTest.cs
+++ Utsusu/Assets/YamamotoOnly/script/stagecriateTest.cs
@@ -2,11 +2,11 @@
 using System.Collections;
 
 public class stagecriateTest : MonoBehaviour {
-    public GameObject block;
+    public GameObject block,Clearzone;
     public int startX=3, startY=3, massWidth=3, massHeighth=3;
-    string[] stage = {
-                        "b    bb   b",
-                        "bbbbbbbbbbb" };
+    string[] stage = {" b b  bb",
+                        "b    b   b  b",
+                        "bbbbbbbbbbbbbbbbbbbbbbbbbbcccccccccc" };
 
     void Start()
     {
@@ -17,6 +17,10 @@ public class stagecriateTest : MonoBehaviour {
                 if (stage[i].Substring(j, 1) == "b")
                 {
                     Instantiate(block, new Vector3(startX + j * massWidth, startY - i * massHeighth, 0.0f), Quaternion.identity);
+                }
+                if (stage[i].Substring(j, 1) == "c")
+                {
+                    Instantiate(Clearzone, new Vector3(startX + j * massWidth, startY - i * massHeighth, 0.0f), Quaternion.identity);
                 }
             }
         }
