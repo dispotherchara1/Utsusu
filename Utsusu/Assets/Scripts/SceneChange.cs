ba5206b37461270496c94559d4ca3be92ffa3a2a
Eita Honjo <nkc.eita.honjo@gmail.com>
2018-06-20 13:38:23 +0900

    コミット忘れてました

--- Utsusu/Assets/Scripts/SceneChange.cs
+++ Utsusu/Assets/Scripts/SceneChange.cs
@@ -4,60 +4,9 @@ using UnityEngine.UI;
 
 public class SceneChange : MonoBehaviour
 {
+    SelectText selectText;
     stagecriate stagecriate;//LoadScene(4)はゲームシーンに飛ぶよ。
-    int selectNum = 0;
-    public Text text1, text2, text3, text4,
-                text5, text6, text7, text8;
-    enum StageSelectType
-    {
-        Easy,
-        Normal,
-        Hard,
-    };
-    StageSelectType stageselectType;
-
-    void Update()
-    {
-        switch (stageselectType)
-        {
-            case StageSelectType.Easy:
-                selectNum = 0;
-                text1.text = "1";
-                text2.text = "2";
-                text3.text = "3";
-                text4.text = "4";
-                text5.text = "5";
-                text6.text = "6";
-                text7.text = "7";
-                text8.text = "8";
-                break;
-
-            case StageSelectType.Normal:
-                selectNum = 8;
-                text1.text = "9";
-                text2.text = "10";
-                text3.text = "11";
-                text4.text = "12";
-                text5.text = "13";
-                text6.text = "14";
-                text7.text = "15";
-                text8.text = "16";
-                break;
-
-            case StageSelectType.Hard:
-                selectNum = 16;
-                text1.text = "17";
-                text2.text = "18";
-                text3.text = "19";
-                text4.text = "20";
-                text5.text = "21";
-                text6.text = "22";
-                text7.text = "23";
-                text8.text = "24";
-                break;
-        }
-    }
-
+    
     public void Select()
     {
         SceneManager.LoadScene("select");
@@ -68,67 +17,57 @@ public class SceneChange : MonoBehaviour
         SceneManager.LoadScene("title");
     }
 
+    public void Tutorial()
+    {
+        SceneManager.LoadScene(5);
+    }
+
     public void Stage()
     {
         SceneManager.LoadScene(4);
-        stagecriate.GetStageNum(1 + selectNum);
+        stagecriate.GetStageNum(1 + SelectText.GetSelectNum());
+        selectText.SetSelectNum(1);
     }
 
     public void Stage2()
     {
         SceneManager.LoadScene(4);
-        stagecriate.GetStageNum(2 + selectNum);
+        stagecriate.GetStageNum(2 + SelectText.GetSelectNum());
     }
 
     public void Stage3()
     {
         SceneManager.LoadScene(4);
-        stagecriate.GetStageNum(3 + selectNum);
+        stagecriate.GetStageNum(3 + SelectText.GetSelectNum());
     }
 
     public void Stage4()
     {
         SceneManager.LoadScene(4);
-        stagecriate.GetStageNum(4 + selectNum);
+        stagecriate.GetStageNum(4 + SelectText.GetSelectNum());
     }
 
     public void Stage5()
     {
         SceneManager.LoadScene(4);
-        stagecriate.GetStageNum(5 + selectNum);
+        stagecriate.GetStageNum(5 + SelectText.GetSelectNum());
     }
 
     public void Stage6()
     {
         SceneManager.LoadScene(4);
-        stagecriate.GetStageNum(6 + selectNum);
+        stagecriate.GetStageNum(6 + SelectText.GetSelectNum());
     }
-
+    
     public void Stage7()
     {
         SceneManager.LoadScene(4);
-        stagecriate.GetStageNum(7 + selectNum);
+        stagecriate.GetStageNum(7 + SelectText.GetSelectNum());
     }
 
     public void Stage8()
     {
         SceneManager.LoadScene(4);
-        stagecriate.GetStageNum(8 + selectNum);
-    }
-
-    public void Next()
-    {
-        if (stageselectType < StageSelectType.Hard)
-        {
-            stageselectType++;
-        }
-    }
-    
-    public void Back()
-    {
-        if (stageselectType > StageSelectType.Easy)
-        {
-            stageselectType--;
-        }
+        stagecriate.GetStageNum(8 + SelectText.GetSelectNum());
     }
 }
