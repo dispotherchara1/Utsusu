db2c452037fcc0dd6c1c422be3c60ec1e9c041b8
Rikiya Yamamoto <nkc.yamamoto.rikiya@gmail.com>
2018-06-22 12:14:49 +0900

    背景のオンオフ切り替えを作った。 沢山切り替えられたらなぁ、と思うほど背景は無い。

---
+++ Utsusu/Assets/Scripts/BGPmanagemant.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class BGPmanagemant : MonoBehaviour
+{
+    public GameObject[] BGP;
+    private void Start()
+    {
+        BGP[0].SetActive(false);
+    }
+}
