b03024aa840f5e41969e4b86617551b778e40283
Rikiya Yamamoto <nkc.yamamoto.rikiya@gmail.com>
2018-06-22 12:46:01 +0900

    賢そうなコードを思案中

--- Utsusu/Assets/Scripts/BGPmanagemant.cs
+++ Utsusu/Assets/Scripts/BGPmanagemant.cs
@@ -3,9 +3,26 @@ using System.Collections;
 using UnityEngine.UI;
 public class BGPmanagemant : MonoBehaviour
 {
+
     public GameObject[] BGP;
+    public mynumber MyNum;
+
     private void Start()
     {
         BGP[0].SetActive(false);
+        BGP[1].SetActive(false);
+    }
+    private void Update()
+    {
+        if (MyNum.GetstageNum() >= 8)
+        {
+            BGP[1].SetActive(false);
+            BGP[0].SetActive(true);
+        }
+        if (MyNum.GetstageNum() <= 9&&MyNum.GetstageNum()>=16)
+        {
+            BGP[0].SetActive(false);
+            BGP[1].SetActive(true);
+        }
     }
 }
