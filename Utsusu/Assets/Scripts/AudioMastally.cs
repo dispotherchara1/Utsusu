689e0d72d8985b9c9850fbb37c41fd3c9975c2fd
Rikiya Yamamoto <nkc.yamamoto.rikiya@gmail.com>
2018-06-27 14:28:12 +0900

    ホラゲー並みに不気味なBGMが付いてしまった

---
+++ Utsusu/Assets/Scripts/AudioMastally.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioMastally : MonoBehaviour {
+
+    public AudioSource[] sources;
+    // Use this for initialization
+    void Start () {
+        sources[0].Play();//音1
+        sources[1].Play();//音2
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
