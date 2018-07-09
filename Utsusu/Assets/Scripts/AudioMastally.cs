37cb2bdeda90440a10bb4d761af4e3c47c7dd093
Eita Honjo <nkc.eita.honjo@gmail.com>
2018-07-09 14:20:24 +0900

    調整

--- Utsusu/Assets/Scripts/AudioMastally.cs
+++ Utsusu/Assets/Scripts/AudioMastally.cs
@@ -1,17 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AudioMastally : MonoBehaviour {
-
+public class AudioMastally : MonoBehaviour
+{
     public AudioSource[] sources;
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
         sources[0].Play();//音1
         //sources[1].Play();//音2
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
